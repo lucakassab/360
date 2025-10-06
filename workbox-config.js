69d0fd1db0d6e5af11133ff58dabaaa160a6de56
lucakassab <48773596+lucakassab@users.noreply.github.com>
2025-10-06 15:00:08 -0300

    Delete workbox-config.js

--- workbox-config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  swSrc: 'sw.js',        // Arquivo Service Worker de origem
-  swDest: 'sw.js',       // Arquivo Service Worker resultante
-  globDirectory: './',   // Diretório base para busca de assets
-  globPatterns: [        // Padrões de arquivos a serem injetados no precache
-    '**/*.{html,js,css,webmanifest,png,ico}'
-  ],
-};
